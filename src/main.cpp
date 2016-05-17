63a9569360fd7490f4cf688c4b9e2fa4d071449e
Simon Repp <simon@fdpl.foundation>
2016-05-17 18:02:39 +0200

    Remove static plugin import for linux platform

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,6 @@
 #include <QtPlugin>
 #include <QScreen>
 
-#if defined(Q_OS_LINUX) && defined(Q_NO_DEBUG)
-Q_IMPORT_PLUGIN(QGtk2ThemePlugin)
-Q_IMPORT_PLUGIN(QXcbIntegrationPlugin)
-#endif
-
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
